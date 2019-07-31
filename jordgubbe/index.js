12f21a3b4193093c35a79265d0fff7ebf079d829
Peter Meehan <peter.meehan@intercom.io>
2019-07-31 14:51:02 +0100

    update script with classlist

--- jordgubbe/index.js
+++ jordgubbe/index.js
@@ -1,19 +1,18 @@
-// try rendering this in an iframe in the figma plugin
-//
-//
 // domain knowledge baked in - here we reference some Pulse playground internals
 // this script will only work when executed inside the `/render` iframe
-let elem = document.querySelector('.render-play').firstElementChild.firstElementChild
+let elem = document.querySelector(".render-play").firstElementChild
+  .firstElementChild;
 
-const normalise = o => JSON.parse(JSON.stringify(o))
+const normalise = o => JSON.parse(JSON.stringify(o));
 
 const buildAST = node => ({
   tagName: node.tagName,
   innerText: node.innerText,
+  classList: Array.from(node.classList),
   computedStyle: normalise(window.getComputedStyle(node)),
   // TODO: capture computed styles for pseudo elements and hover / clicked states?
   boundingClientRect: normalise(node.getBoundingClientRect()),
   children: Array.from(node.children).map(buildAST)
-})
+});
 
-console.log(buildAST(elem))
+console.log(JSON.stringify(buildAST(elem)));
